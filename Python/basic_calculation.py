5c7d00dbca54caf089f585f85c6e3fea276e594a
Hao Ma <mahao.thume@gmail.com>
2018-07-04 12:13:29 +0800

    add quaternion calculation notes

---
+++ Python/basic_calculation.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Demo code for basic quaternion calculation
+
+Created on Wed Jul  4 10:13:01 2018
+
+@author: mahao
+"""
+
+
---
+++ Python/kinematic_chain.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Demo code for kinematic chain calculation using quaternion
+
+Created on Wed Jul  4 10:15:05 2018
+
+@author: NOITOM
+"""
+
