c06003a26833a6426d92d31bb539f0ecb3003fa7
Hao Ma <mahao.thume@gmail.com>
2018-07-09 17:24:05 +0800

    update Matlab example

--- Matlab/basic_calculation.m
+++ Matlab/basic_calculation.m
@@ -12,7 +12,7 @@ addpath('functions')
 % 1. Quaternion construction
 % Rotation from A to B:
 r1_A = [0.0 0.0 1.0];
-theta1 = D2R(180.0);
+theta1 = D2R(90.0);
 % Rotation from A to C:
 r2_A = [0.0 1.0 0.0];
 theta2 = D2R(90.0);
@@ -22,23 +22,26 @@ q_A_C = axangle2quat(r2_A,theta2)
 
 % 2. Vector coordinate transformation
 xA_A = [1.0 0.0 0.0];
-xA_B = quatrotate(xA_A,q_A_B)
-xA_C = quatrotate(xA_A,q_A_C)
+xA_B = quatrotate(xA_A,q_A_B);
+xA_B = xA_B(2:end)
+xA_C = quatrotate(xA_A,q_A_C);
+xA_C = xA_C(2:end)
 
 q_B_C = quatmultiply(q_A_C,quatinv(q_A_B))
 xB_B = [1.0 0.0 0.0];
-xB_C = quatrotate(xB_B,q_B_C)
+xB_C = quatrotate(xB_B,q_B_C);
+xB_C = xB_C(2:end)
 
 % 3. Quatrenion and rotational matrix conversion
 M_A_B_1 = quat2rotm(q_A_B)
-yA_B = [0.0 0.0 -1.0 0.0];
-zA_B = [0.0 0.0 0.0 1.0];
+yA_B = [1.0 0.0 0.0];
+zA_B = [0.0 0.0 1.0];
 M_A_B_2 = [xA_B;yA_B;zA_B]'
 
 M_A_C_1 = quat2rotm(q_A_C)
-yA_C = [0.0 0.0 1.0 0.0];
-zA_C = [0.0 -1.0 0.0 0.0];
-M_A_C_2=[xA_C;yA_C;zA_C]'
+yA_C = [0.0 1.0 0.0];
+zA_C = [-1.0 0.0 0.0];
+M_A_C_2 = [xA_C;yA_C;zA_C]'
 
 % xA_A to xA_B & xA_A to xA_C
 xA_B = M_A_B_1 * xA_A'
@@ -46,15 +49,18 @@ xA_C = M_A_C_1 * xA_A'
 
 %M_B_C
 M_B_C = M_A_C_1 * (M_A_B_1)'
+yB_C = [0.0, 0.0, -1.0];
+zB_C = [-1.0, 0.0, 0.0];
+M_B_C_2 = [xB_C;yB_C;zB_C]'
 
 % 4. Quaternion/Rotational matrix and Euler angle conversion
 % rotate B by gamma, beta, alpha with 'ZYX' order to get C
 alpha = D2R(0.0);  % rotation angle around x axis
 beta = D2R(90.0);  % rotation angle around y axis
-gamma = D2R(180.0);  % rotation angle around z axis
-q_B_C = angle2quat(gamma, beta, alpha, 'ZYX')
-quatinv(q_B_C)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-[gamma_res, beta_res, alpha_res] = quat2angle(q_B_C, 'ZYX');
+gamma = D2R(-90.0);  % rotation angle around z axis
+q_B_C = quatinv(angle2quat(gamma, beta, alpha, 'ZYX'))
+
+[gamma_res, beta_res, alpha_res] = quat2angle(quatinv(q_B_C), 'ZYX');
 R2D(gamma_res)
 R2D(beta_res)
 R2D(alpha_res)
