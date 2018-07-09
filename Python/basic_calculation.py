2658290edf4877690c6f7c28cdad4cfb39cc04e5
Hao Ma <mahao.thume@gmail.com>
2018-07-09 17:36:11 +0800

    update Python example and correct Euler Angle bugs

--- Python/basic_calculation.py
+++ Python/basic_calculation.py
@@ -2,8 +2,6 @@
 """
 Demo code for basic quaternion calculation
 
-Created on Wed Jul  4 10:13:01 2018
-
 @author: mahao
 """
 
@@ -22,13 +20,13 @@ def D2R(deg):
      
 # Define three coordinate systems (CS):
 # A: x - East, y - North, z - Up
-# B: x - West, y - South, z - Up   (A rotate around its z axis with 180 degree)
-# C: x - Down, y - North, z - East (A rotate around its y axis with 90  degree)
+# B: x - North, y - West, z - Up   (A rotate around its z axis with 90 degree)
+# C: x - Down, y - North, z - East (A rotate around its y axis with 90 degree)
     
 # 1. Quaternion construction
 # Rotation from A to B:
 r1_A = np.array([0.0, 0.0, 1.0])  # rotation axis: z axis of A  
-theta1 = D2R(180.0)  # rotation angle (following right-hand rule)
+theta1 = D2R(90.0)  # rotation angle (following right-hand rule)
 
 # Rotation from A to C:
 r2_A = np.array([0.0, 1.0, 0.0])  # rotaion axis: y axis of A
@@ -63,7 +61,7 @@ print(xB_C)
 # 3. Quatrenion and rotational matrix conversion
 M_A_B_1 = quat.quat2mat(q_A_B)
 M_A_B_2 = np.eye(3)
-yA_B = np.array([0.0, -1.0, 0.0])
+yA_B = np.array([1.0, 0.0, 0.0])
 zA_B = np.array([0.0, 0.0, 1.0])
 M_A_B_2[:,0] = xA_B
 M_A_B_2[:,1] = yA_B
@@ -87,7 +85,7 @@ print(M_A_C_2)
 
 M_B_C_1 = M_A_C.dot(M_A_B.T);
 M_B_C_2 = np.eye(3)
-yB_C = np.array([0.0, -1.0, 0.0]);
+yB_C = np.array([0.0, 0.0, -1.0]);
 zB_C = np.array([-1.0, 0.0, 0.0]);
 M_B_C_2[:,0] = xB_C
 M_B_C_2[:,1] = yB_C
@@ -100,13 +98,13 @@ print(M_B_C_2)
 # 4. Quaternion/Rotational matrix and Euler angle conversion
 alpha = D2R(0.0)  # rotation angle around x axis
 beta = D2R(90.0)  # rotation angle around y axis
-gamma = D2R(180.0)  # rotation angle around z axis
+gamma = D2R(-90.0)  # rotation angle around z axis
 M_B_C_3 = np.eye(3)
-M_B_C_3 = euler.euler2mat(gamma, beta, alpha, 'szyx')
+M_B_C_3 = (euler.euler2mat(gamma, beta, alpha, 'rzyx')).T
 print("M_B_C: ")
 print(M_B_C_3)
-euler_angles = euler.mat2euler(M_B_C_3, 'szyx')
-print("euler angles in ZYZ order: ")
+euler_angles = euler.mat2euler(M_B_C_3.T, 'rzyx')
+print("euler angles in ZYX order: ")
 print(R2D(gamma), R2D(beta), R2D(alpha))
 print([R2D(angle) for angle in euler_angles])
 
